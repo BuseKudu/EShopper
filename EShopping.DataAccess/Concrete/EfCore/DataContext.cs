cacbe9352a39b2a7a440c09c56a3f156bd63e0f2
BuseKudu <94551478+BuseKudu@users.noreply.github.com>
2022-04-21 01:29:53 +0300

    Add project files.

---
+++ EShopping.DataAccess/Concrete/EfCore/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using EShopper.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopper.DataAccess.Concrete.EfCore
+{
+    public class DataContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connection =>)
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ EShopping.DataAccess/Concrete/EfCore/EfCoreProductDal.cs
@@ -0,0 +1,19 @@
+﻿using EShopper.DataAccess.Abstract;
+using EShopper.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopper.DataAccess.Concrete.EfCore
+{
+    public class EfCoreProductDal : EfCoreGenericRepository<Product, DataContext>, IProductDal
+    {
+        public IEnumerable<Product> GetPopularProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
